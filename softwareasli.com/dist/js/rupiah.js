1095806db5507089dc16245b08388822fd4253ec
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2020-04-21 12:47:16 +0700

    Add files via upload

---
+++ softwareasli.com/dist/js/rupiah.js
@@ -0,0 +1,39 @@
+function convertToRupiah(objek) {
+	  separator = ".";
+	  a = objek.value;
+	  b = a.replace(/[^\d]/g,"");
+	  c = "";
+	  panjang = b.length; 
+	  j = 0; 
+	  for (i = panjang; i > 0; i--) {
+	    j = j + 1;
+	    if (((j % 3) == 1) && (j != 1)) {
+	      c = b.substr(i-1,1) + separator + c;
+	    } else {
+	      c = b.substr(i-1,1) + c;
+	    }
+	  }
+	  objek.value = c;
+	}       
+
+function convertToRupiahNow(angka) {
+	var rupiah = '';		
+	var angkarev = angka.toString().split('').reverse().join('');
+	for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+	return 'Rp. '+rupiah.split('',rupiah.length-1).reverse().join('');
+}
+function convertToUsd(number) {
+	return Number(number).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+	function convertToAngka()
+	{	var nominal= document.getElementById("nominal").value;
+		var angka = parseInt(nominal.replace(/,.*|[^0-9]/g, ''), 10);
+		document.getElementById("angka").innerHTML= angka;
+	}       
+
+	function convertToAngka()
+	{	var nominal1= document.getElementById("nominal1").value;
+		var angka1 = parseInt(nominal.replace(/,.*|[^0-9]/g, ''), 10);
+		document.getElementById("angka1").innerHTML= angka;
+	}
